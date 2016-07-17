76570f829402036af70002c352ee867dfd220411
Sukalpo Mitra <sukalpomitra@gmail.com>
2016-07-17 13:30:00 +0800

    def apply

--- ImmutablelinkedList/src/main/scala/linkedlistexample/List.scala
+++ ImmutablelinkedList/src/main/scala/linkedlistexample/List.scala
@@ -20,6 +20,14 @@ class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty = false
 }
 
+//how to write a function List(1, 2) that gives a list of two elements
+object List {
+  //List(1,2) = List.apply(1,2)
+  def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+  def apply[T]() = new Nil
+
+}
+
 object Test extends App {
   def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
 
